966ebaf57a4f8d370812394377e6554f460a3d74
tamurayk <tamura.yukio@parente.jp>
2018-03-17 16:21:56 +0900

    add comment

--- hellojscafe.js
+++ hellojscafe.js
@@ -1,7 +1,13 @@
+// httpモジュールを読み込み、httpという同名の変数に格納しています。
+// requireはモジュール読み込みのためのグローバルモジュールです。
+// requireを記述する時は慣例として、ファイルの先頭に記述することが多いです。
 var http = require('http');
 
+// httpモジュールのメソッドである、createServerを呼出し、
+// サーバーを作成します。
 var server = http.createServer(function(req, res) {
     res.end("Hello JSCafe");
 });
 
+// ポート番号3000番で受け付けるサーバーを起動する。
 server.listen(3000);
